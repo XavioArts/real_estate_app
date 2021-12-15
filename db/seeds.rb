5777f4b0377a807d8a776f8fd0e3ab0ca6f6c072
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-15 10:13:22 -0700

    back end set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u1 = User.create(email: "zwarriors@test.com", password: 123456);
+
+p u1
