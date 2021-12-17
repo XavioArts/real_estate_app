2fc5307d45538b6b2e975ed8167e974cf733768b
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-17 00:04:00 -0700

    bug fixed. template DONE

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,12 +7,16 @@ const Home = () => {
     const auth = useContext(AuthContext);
     const navigate = useNavigate();
 
+    // const logOut = (e) => {
+    //     e.preventDefault()
+    // }
+
     return (
         <div>
             <h1>Home</h1>
             {JSON.stringify(auth)}
             <button onClick={()=>navigate("/login")} >Log In</button>
-            <button onClick={auth.handleLogout} >Log Out</button>
+            <button onClick={()=>auth.handleLogout(navigate)} >Log Out</button>
             <button onClick={()=>navigate("/protected")}>User View</button>
         </div>
     );
--- client/src/pages/Protected.js
+++ client/src/pages/Protected.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Protected = () => {
+
+    const navigate = useNavigate();
+    const auth = useContext(AuthContext);
+
     return (
         <div>
             <h1>Protected</h1>
+            <button onClick={()=>auth.handleLogout(navigate)} >Log Out</button>
         </div>
     );
 };
