40a4fef200186de2359b2b23f91b09baeac4698f
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-24 13:49:25 -0700

    updated authProvider

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const AuthContext = React.createContext();
@@ -10,6 +10,31 @@ const AuthProvider = (props) => {
     const [user, setUser] = useState(null);
     // The user state will keep track of the user that is logged in
     // We initialize the state as null, which is a user that is not logged in
+
+    const [checkingAuthStatus, setCheckingAuthStatus] = useState(true);
+
+    useEffect(()=>{
+        getUser();
+    },[]);
+
+    const getUser = async () => {
+        if (user || !localStorage.getItem("access-token")) {
+            console.log("authenticated");
+            setCheckingAuthStatus(false);
+            return;
+        }
+        try {
+            console.log("validating token");
+            const res = await axios.get("/api/auth/validate_token");
+            setUser(res.data.data);
+        } catch (err) {
+            console.log(err.respose);
+            console.log("unable to validate token");
+        } finally {
+            setCheckingAuthStatus(false);
+            return;
+        }
+    };
 
     const handleRegister = async (user, navigate) => {
         // axios call to register new user
@@ -49,6 +74,30 @@ const AuthProvider = (props) => {
         }
     };
 
+    const handleUpdate = async (user, navigate) => {
+        try {
+            let res = await axios.put("/api/auth", user);
+            console.log(res.data);
+            setUser(res.data.data);
+            navigate("/");
+        }   catch (err) {
+            console.log(err.response);
+            alert("an error occurred updating user")
+        }
+    }
+
+    const handleDelete = async (navigate) => {
+        try {
+            let res = await axios.delete("/api/auth")
+            console.log(res);
+            setUser(null);
+            navigate("/public");
+        } catch (err) {
+            console.log(err.response);
+            alert("there was an error deleting your account");
+        }
+    }
+
     return (
         <AuthContext.Provider value={{
             ...user,
@@ -57,6 +106,9 @@ const AuthProvider = (props) => {
             handleRegister,
             handleLogin,
             handleLogout,
+            handleDelete,
+            handleUpdate,
+            checkingAuthStatus,
         }} >
             {props.children}
         </AuthContext.Provider>
